98f0402994e4cdefc897e45068701d19740ff8d5
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2019-01-21 15:05:20 -0800

    final release

--- ConnectFour/Piece.swift
+++ ConnectFour/Piece.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2019 Cyril Garcia. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Piece: UIButton {
+    
+    var row: Int!
+    var column: Int!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        self.backgroundColor = UIColor.clear
+        self.layer.cornerRadius = self.frame.width / 2
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 2
+    }
+    
+}
